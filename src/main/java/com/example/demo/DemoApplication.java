9723f6940c199800510dc1010a8b8aa7721231ce
tangchao <571108856@qq.com>
2022-01-06 00:17:29 +0800

    [tickNumber]:123456 [describe]:提交代码的验证 [explain]:NA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-
+        System.out.println(new Date());
         SpringApplication.run(DemoApplication.class, args);
     }
 }
